5a167d6f019504f5fd12ff78219fa9dc51d147cb
PegahNa <pegah_uk@yahoo.co.uk>
2023-06-13 22:54:19 +0100

    generateMessages modified

--- generateMessages.js
+++ generateMessages.js
@@ -1,9 +1,19 @@
-const names = ["Anna", "Laura", "Josh", "Min", "Karla"];
+const namesAndDiscounts = [
+  { name: "Anna", discount: 50 },
+  { name: "Laura", discount: 40 },
+  { name: "Josh", discount: 30 },
+  { name: "Min", discount: 50 },
+  { name: "Karla", discount: 60 },
+];
 
-const generateMessages = (names) => {
-  return names.map((name) => {
-    return "Hi " + name + "! 50% off our best candies for you today!";
-  });
-};
+function generateMessages(data) {
+  return data.map(
+    (item) =>
+      `Hi ${item.name}! ${item.discount}% off our best candies for you today!`
+  );
+}
+
+const messages = generateMessages(namesAndDiscounts);
+console.log(messages);
 
 module.exports = generateMessages;
