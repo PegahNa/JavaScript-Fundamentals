98ef5137db35c87fb054009a19b8d7d8e22161af
PegahNa <pegah_uk@yahoo.co.uk>
2023-06-13 23:15:14 +0100

    UserBase

---
+++ userbase.js
@@ -0,0 +1,17 @@
+class UserBase {
+  constructor(users) {
+    this.users = users;
+  }
+
+  count() {
+    this.count = count;
+  }
+
+  getName() {
+    return this.name;
+  }
+
+  getIntroduction() {
+    return "Hi, my name is ${this.name";
+  }
+}
