9f18840ad0585ff551ec1fb12588cce3f39a27ab
PegahNa <pegah_uk@yahoo.co.uk>
2023-06-13 22:27:56 +0100

    generateMessages

---
+++ generateMessages.js
@@ -0,0 +1,9 @@
+const names = ["Anna", "Laura", "Josh", "Min", "Karla"];
+
+const generateMessages = (names) => {
+  return names.map((name) => {
+    return "Hi " + name + "! 50% off our best candies for you today!";
+  });
+};
+
+module.exports = generateMessages;
